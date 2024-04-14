e33a7f9efd12e61a0d731246fa1bdfd908e3ad3c
H4R5H <151930114+g0j0-H4R5H@users.noreply.github.com>
2024-04-14 12:31:55 +0530

    Update views.py

--- chall_app/views.py
+++ chall_app/views.py
@@ -6,35 +6,42 @@ def moshi(request):
     if request.method == 'POST':
         command1 = request.POST.get('command1', '')
         command2 = request.POST.get('command2', '')
-        
+
         if command1:
             if any(c.isalpha() for c in command1):
-                return render(request, 'baakaa.html', {'output1': f'{command1} Alphabets are not allowed in the command'})
-            elif '. ./*/*' in command1:
-                return render(request, 'baakaa.html', {'output1': f'{command1} Hehe not allowed'})
+                return render(request, 'baakaa.html', {'output1': f'"{command1}" Not allowed in the command'})
+            elif any(c in ['. ' , ' ' , '<.' , '<~'] for c in command1):
+                return render(request, 'baakaa.html', {'output1': f'"{command1}" Hehe not allowed'})
+            elif command1.isdecimal():
+                output1 = int(command1)*2
             else:
                 try:
                     current_directory = os.path.dirname(os.path.abspath(__file__))
                     # Set the working directory to the 'pro-player' folder
                     working_directory = os.path.join(current_directory, '..', 'pro-player')
-                    output1 = subprocess.check_output(command1, shell=True, stderr=subprocess.STDOUT, cwd=working_directory, encoding='utf-8')
-                    return render(request, 'baakaa.html', {'output1': output1.decode('utf-8')})
-                except subprocess.CalledProcessError as e:
-                    return render(request, 'baakaa.html', {'output1': f'Your 1st input is {command1}'})
-                
+                    output1 = subprocess.Popen(
+                    ["/bin/bash", "-c", command1], stdout=subprocess.PIPE, stderr=subprocess.STDOUT,cwd=working_directory)
+                    output1 = output1.stdout.read().decode('utf-8')
+                except Exception:
+                    return render(request, 'baakaa.html', {'output1': f'Error executing command1: {command1}'})
+
         if command2:
-            if any(c.isalpha() for c in command2):
-                return render(request, 'baakaa.html', {'output2': f'{command2} Alphabets are not allowed in the command'})
-            elif len(command2) >= 8:
-                return render(request, 'baakaa.html', {'output2': f'{command2} too much long'})
+            if any(c.isalpha() for c in command2) or " " in command2:
+                return render(request, 'baakaa.html', {'output2': f'"{command2}" Not allowed in the command'})
+            elif len(command2) > 8:
+                return render(request, 'baakaa.html', {'output2': f'"{command2}" too much long'})
+            elif command2.isdecimal():
+                output2 = int(command2)*2
             else:
                 try:
+                    command2 = command2.replace("||"," ")
                     current_directory = os.path.dirname(os.path.abspath(__file__))
                     # Set the working directory to the 'noob-player' folder
                     working_directory = os.path.join(current_directory, '..', 'noob-player')
-                    output2 = subprocess.check_output(command2, shell=True, stderr=subprocess.STDOUT,cwd=working_directory, encoding='utf-8')
-                    return render(request, 'baakaa.html', {'output2': output2.decode('utf-8')})
-                except subprocess.CalledProcessError as e:
-                    return render(request, 'baakaa.html', {'output2': f'Your 2nd input is {command2}'})
-                
+                    output2 = subprocess.Popen(
+                    ["/bin/bash", "-c", command2], stdout=subprocess.PIPE, stderr=subprocess.STDOUT,cwd=working_directory)
+                    output2 = output2.stdout.read().decode('utf-8')
+                except Exception:
+                    return render(request, 'baakaa.html', {'output2': f'Error executing command2: {command2}'})
+        return render(request, 'baakaa.html', {'output1': output1,'output2':output2})
     return render(request, 'home.html')
